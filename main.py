e379dbfeaed30961bb27889552ae34040eaa2f77
Udayraj Sahu <udayrajsahu098@gmail.com>
2024-09-02 19:16:28 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarization.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarization.logging import logger
 
 
@@ -7,6 +8,19 @@ try:
     logger.info(f">>>>>> Stage {STAGE_NAME} started")
     dataingestion = DataIngestionTrainingPipeline()
     dataingestion.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started")
+    datavalidation = DataValidationTrainingPipeline()
+    datavalidation.main()
     logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<")
 except Exception as e:
     logger.exception(e)
---
+++ src/textSummarization/pipeline/stage_02_data_validation.py
@@ -0,0 +1,14 @@
+from textSummarization.config.configuration import ConfigurationManager
+from textSummarization.components.data_validation import DataValidation
+from textSummarization.logging import logger
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+
+    def main(self):
+          config = ConfigurationManager()
+          data_validation_config = config.get_data_validation_config()
+          data_validation = DataValidation(config=data_validation_config)
+          data_validation.validate_all_files_exist()
